b66561219008a8664b74fb3b64a1bc3d0eabc1c3
James Hillyerd <james@hillyerd.com>
2012-10-25 18:11:27 -0700

    Start retention code/unit tests

---
+++ smtpd/retention.go
@@ -0,0 +1,12 @@
+package smtpd
+
+import (
+	//"github.com/jhillyerd/inbucket/config"
+	//"github.com/jhillyerd/inbucket/log"
+//	"io/ioutil"
+)
+
+// retentionScan does a single pass of all mailboxes looking for messages that can be purged
+func retentionScan(ds *DataStore) {
+
+}

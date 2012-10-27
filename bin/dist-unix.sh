9ee8a2e1e3abeed2d32f7d41be8bd5b851252cb1
James Hillyerd <james@hillyerd.com>
2012-10-27 12:09:27 -0700

    Tarball building script for unix

---
+++ bin/dist-unix.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Compile and package inbucket dist for unix
+
+if [ "$1x" == "x" ]; then
+  echo "Usage: $0 <version-label>" 1>&2
+  exit 1
+fi
+label="$1"
+
+# Bail on error
+set -e
+
+# Work directory
+tmpdir=/tmp/inbucket-dist.$$
+mkdir -p $tmpdir
+
+
+# Figure out our build env/target
+go env > $tmpdir/env
+source $tmpdir/env
+distname="inbucket-${label}-${GOOS}_$GOARCH"
+distdir="$tmpdir/$distname"
+
+echo "Building $distname..."
+mkdir -p $distdir
+go build -o $distdir/inbucket -a -v github.com/jhillyerd/inbucket
+
+echo "Copying resources..."
+cp LICENSE README.md $distdir/
+cp -r etc $distdir/etc
+cp -r themes $distdir/themes
+
+echo "Tarballing..."
+tarball="$HOME/$distname.tbz2"
+cd $tmpdir
+tar cjvf $tarball $distname
+
+echo "Created $tarball"

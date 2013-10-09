dcc5d240cb6e3c7832849d8b67c0d26a66273d14
James Hillyerd <james@hillyerd.com>
2013-10-09 16:20:13 -0700

    Add listener configuration details to status page

--- web/root_controller.go
+++ web/root_controller.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"github.com/jhillyerd/inbucket/config"
 	"net/http"
 )
@@ -13,8 +14,17 @@ func RootIndex(w http.ResponseWriter, req *http.Request, ctx *Context) (err erro
 
 func RootStatus(w http.ResponseWriter, req *http.Request, ctx *Context) (err error) {
 	retentionMinutes := config.GetDataStoreConfig().RetentionMinutes
+	smtpListener := fmt.Sprintf("%s:%d", config.GetSmtpConfig().Ip4address.String(),
+		config.GetSmtpConfig().Ip4port)
+	pop3Listener := fmt.Sprintf("%s:%d", config.GetPop3Config().Ip4address.String(),
+		config.GetPop3Config().Ip4port)
+	webListener := fmt.Sprintf("%s:%d", config.GetWebConfig().Ip4address.String(),
+		config.GetWebConfig().Ip4port)
 	return RenderTemplate("root/status.html", w, map[string]interface{}{
-		"ctx": ctx,
+		"ctx":              ctx,
 		"retentionMinutes": retentionMinutes,
+		"smtpListener":     smtpListener,
+		"pop3Listener":     pop3Listener,
+		"webListener":      webListener,
 	})
 }

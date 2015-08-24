44ff0be01ebe6f81e568431f14b2e822ec849b5e
James Hillyerd <james@hillyerd.com>
2015-08-24 12:33:50 -0700

    Add a gmail message to test with

--- swaks-tests/run-tests.sh
+++ swaks-tests/run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# run-tests.sh: Generate test emails for Inbucket
 
 export SWAKS_OPT_server="127.0.0.1:2500"
 export SWAKS_OPT_to="swaks@inbucket.local"
@@ -16,4 +17,7 @@ swaks $* --h-Subject: "Swaks Top Level HTML" --data nonmime-html.raw
 swaks $* --h-Subject: "Swaks Attachment" --attach-type image/png --attach favicon.png --body text.txt
 
 # Encoded subject line test
-swaks $* --data utf8-subject.txt
+swaks $* --data utf8-subject.raw
+
+# Gmail test
+swaks $* --data gmail.raw

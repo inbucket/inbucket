fd59aad4f06aef2c52fb1262dee125fa59e8585b
James Hillyerd <james@hillyerd.com>
2016-03-01 00:10:10 -0800

    Fix "Link" button on messages
    - Add clipboard.js to bower
    - Add pop-out section that populates with URL
    - Add copy to clipboard button, hover confirmation

--- themes/bootstrap/public/mailbox.js
+++ themes/bootstrap/public/mailbox.js
@@ -1,17 +1,47 @@
+var baseURL = window.location.protocol + '//' + window.location.host;
 var navBarOffset = 75;
 var mediumDeviceWidth = 980;
 var messageListMargin = 275;
+var clipboard = null;
+
+// onDocChange is called each time we load partials into the DOM
+function onDocChange() {
+  // Bootstrap tooltips
+  $('[data-toggle="tooltip"]').tooltip()
+
+  // Clipboard functionality
+  if (clipboard != null) {
+    clipboard.destroy();
+  }
+  clipboard = new Clipboard('.btn-clipboard');
+  clipboard.on('success', function(el) {
+    flashTooltip(el.trigger, 'Copied!');
+    el.clearSelection();
+  });
+  clipboard.on('error', function(el) {
+    flashTooltip(el.trigger, 'Copy Failed!');
+  });
+}
+
+// flashTooltip temporarily changes the text of a tooltip
+function flashTooltip(el, text) {
+  var prevText = $(el).attr('data-original-title');
+  $(el).attr('data-original-title', text).tooltip('show');
+  $(el).attr('data-original-title', prevText);
+}
 
 function messageLoaded(responseText, textStatus, XMLHttpRequest) {
   if (textStatus == "error") {
     alert("Failed to load message, server said:\n" + responseText);
-  } else {
-    var top = $('#message-container').offset().top - navBarOffset;
-    $(window).scrollTop(top);
+    return;
   }
+  onDocChange();
+  var top = $('#message-container').offset().top - navBarOffset;
+  $(window).scrollTop(top);
 }
 
 function listLoaded() {
+  onDocChange();
   $('.listEntry').click(
       function() {
         $('.listEntry').removeClass("disabled");
@@ -71,5 +101,11 @@ function messageSource(id) {
   window.open('/mailbox/' + mailbox + '/' + id + "/source", '_blank',
       'width=800,height=600,' +
       'menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');
+}
+
+function toggleMessageLink(id) {
+  var url = baseURL + '/link/' + mailbox + '/' + id;
+  $('#link-input-control').val(url);
+  $('#link-row').slideToggle();
 }
 

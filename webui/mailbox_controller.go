c346372c85613df9409db904af0ddf36dc5a7104
James Hillyerd <james@hillyerd.com>
2017-01-21 19:31:39 -0800

    Fix for flashes not clearing bug

--- webui/mailbox_controller.go
+++ webui/mailbox_controller.go
@@ -17,14 +17,12 @@ func MailboxIndex(w http.ResponseWriter, req *http.Request, ctx *httpd.Context) 
 	// Form values must be validated manually
 	name := req.FormValue("name")
 	selected := req.FormValue("id")
-
 	if len(name) == 0 {
 		ctx.Session.AddFlash("Account name is required", "errors")
 		_ = ctx.Session.Save(req, w)
 		http.Redirect(w, req, httpd.Reverse("RootIndex"), http.StatusSeeOther)
 		return nil
 	}
-
 	name, err = smtpd.ParseMailboxName(name)
 	if err != nil {
 		ctx.Session.AddFlash(err.Error(), "errors")
@@ -32,17 +30,19 @@ func MailboxIndex(w http.ResponseWriter, req *http.Request, ctx *httpd.Context) 
 		http.Redirect(w, req, httpd.Reverse("RootIndex"), http.StatusSeeOther)
 		return nil
 	}
-
 	// Remember this mailbox was visited
 	RememberMailbox(ctx, name)
+	// Get flash messages, save session
+	errorFlash := ctx.Session.Flashes("errors")
 	if err = ctx.Session.Save(req, w); err != nil {
 		return err
 	}
-
+	// Render template
 	return httpd.RenderTemplate("mailbox/index.html", w, map[string]interface{}{
-		"ctx":      ctx,
-		"name":     name,
-		"selected": selected,
+		"ctx":        ctx,
+		"errorFlash": errorFlash,
+		"name":       name,
+		"selected":   selected,
 	})
 }
 
@@ -57,7 +57,7 @@ func MailboxLink(w http.ResponseWriter, req *http.Request, ctx *httpd.Context) (
 		http.Redirect(w, req, httpd.Reverse("RootIndex"), http.StatusSeeOther)
 		return nil
 	}
-
+	// Build redirect
 	uri := fmt.Sprintf("%s?name=%s&id=%s", httpd.Reverse("MailboxIndex"), name, id)
 	http.Redirect(w, req, uri, http.StatusSeeOther)
 	return nil
@@ -81,7 +81,7 @@ func MailboxList(w http.ResponseWriter, req *http.Request, ctx *httpd.Context) (
 		return fmt.Errorf("Failed to get messages for %v: %v", name, err)
 	}
 	log.Tracef("Got %v messsages", len(messages))
-
+	// Render partial template
 	return httpd.RenderPartial("mailbox/_list.html", w, map[string]interface{}{
 		"ctx":      ctx,
 		"name":     name,
@@ -115,10 +115,9 @@ func MailboxShow(w http.ResponseWriter, req *http.Request, ctx *httpd.Context) (
 	if err != nil {
 		return fmt.Errorf("ReadBody(%q) failed: %v", id, err)
 	}
-
 	body := template.HTML(httpd.TextToHTML(mime.Text))
 	htmlAvailable := mime.HTML != ""
-
+	// Render partial template
 	return httpd.RenderPartial("mailbox/_show.html", w, map[string]interface{}{
 		"ctx":           ctx,
 		"name":          name,
@@ -156,7 +155,7 @@ func MailboxHTML(w http.ResponseWriter, req *http.Request, ctx *httpd.Context) (
 	if err != nil {
 		return fmt.Errorf("ReadBody(%q) failed: %v", id, err)
 	}
-
+	// Render partial template
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	return httpd.RenderPartial("mailbox/_html.html", w, map[string]interface{}{
 		"ctx":     ctx,
@@ -193,7 +192,7 @@ func MailboxSource(w http.ResponseWriter, req *http.Request, ctx *httpd.Context)
 	if err != nil {
 		return fmt.Errorf("ReadRaw(%q) failed: %v", id, err)
 	}
-
+	// Output message source
 	w.Header().Set("Content-Type", "text/plain")
 	if _, err := io.WriteString(w, *raw); err != nil {
 		return err
@@ -221,7 +220,6 @@ func MailboxDownloadAttach(w http.ResponseWriter, req *http.Request, ctx *httpd.
 		http.Redirect(w, req, httpd.Reverse("RootIndex"), http.StatusSeeOther)
 		return nil
 	}
-
 	mb, err := ctx.DataStore.MailboxFor(name)
 	if err != nil {
 		// This doesn't indicate not found, likely an IO error
@@ -247,7 +245,7 @@ func MailboxDownloadAttach(w http.ResponseWriter, req *http.Request, ctx *httpd.
 		return nil
 	}
 	part := body.Attachments[num]
-
+	// Output attachment
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Header().Set("Content-Disposition", "attachment")
 	if _, err := io.Copy(w, part); err != nil {
@@ -275,7 +273,6 @@ func MailboxViewAttach(w http.ResponseWriter, req *http.Request, ctx *httpd.Cont
 		http.Redirect(w, req, httpd.Reverse("RootIndex"), http.StatusSeeOther)
 		return nil
 	}
-
 	mb, err := ctx.DataStore.MailboxFor(name)
 	if err != nil {
 		// This doesn't indicate not found, likely an IO error
@@ -301,7 +298,7 @@ func MailboxViewAttach(w http.ResponseWriter, req *http.Request, ctx *httpd.Cont
 		return nil
 	}
 	part := body.Attachments[num]
-
+	// Output attachment
 	w.Header().Set("Content-Type", part.ContentType)
 	if _, err := io.Copy(w, part); err != nil {
 		return err

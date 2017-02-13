304a2260e8945d2fa54438bcb7b293d23d9a90f0
James Hillyerd <james@hillyerd.com>
2017-02-12 16:21:44 -0800

    Don't close writers with defer

--- smtpd/filestore.go
+++ smtpd/filestore.go
@@ -254,22 +254,22 @@ func (mb *FileMailbox) writeIndex() error {
 		if err != nil {
 			return err
 		}
-		defer func() {
-			if err := file.Close(); err != nil {
-				log.Errorf("Failed to close %q: %v", mb.indexPath, err)
-			}
-		}()
 		writer := bufio.NewWriter(file)
-
 		// Write each message and then flush
 		enc := gob.NewEncoder(writer)
 		for _, m := range mb.messages {
 			err = enc.Encode(m)
 			if err != nil {
+				_ = file.Close()
 				return err
 			}
 		}
 		if err := writer.Flush(); err != nil {
+			_ = file.Close()
+			return err
+		}
+		if err := file.Close(); err != nil {
+			log.Errorf("Failed to close %q: %v", mb.indexPath, err)
 			return err
 		}
 	} else {

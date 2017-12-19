b28e1d86d8ba39deebf0ddfcf14250cbc80afb9b
James Hillyerd <james@hillyerd.com>
2017-12-18 19:15:51 -0800

    Include version for final goxc release

--- inbucket.go
+++ inbucket.go
@@ -24,7 +24,7 @@ import (
 
 var (
 	// version contains the build version number, populated during linking
-	version = "undefined"
+	version = "1.2.0-rc2"
 
 	// date contains the build date, populated during linking
 	date = "undefined"

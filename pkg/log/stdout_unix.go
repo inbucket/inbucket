2d09e94f873e15da14a6938cff28dab959751de3
James Hillyerd <james@hillyerd.com>
2018-03-25 16:08:34 -0700

    log: Fix another deadlock.

--- pkg/log/stdout_unix.go
+++ pkg/log/stdout_unix.go
@@ -3,8 +3,10 @@
 package log
 
 import (
-	"golang.org/x/sys/unix"
+	"log"
 	"os"
+
+	"golang.org/x/sys/unix"
 )
 
 // closeStdin will close stdin on Unix platforms - this is standard practice
@@ -12,20 +14,19 @@ import (
 func closeStdin() {
 	if err := os.Stdin.Close(); err != nil {
 		// Not a fatal error
-		Errorf("Failed to close os.Stdin during log setup")
+		log.Printf("Failed to close os.Stdin during log setup")
 	}
 }
 
 // reassignStdout points stdout/stderr to our logfile on systems that support
 // the Dup2 syscall per https://github.com/golang/go/issues/325
 func reassignStdout() {
-	Tracef("Unix reassignStdout()")
 	if err := unix.Dup2(int(logf.Fd()), 1); err != nil {
 		// Not considered fatal
-		Errorf("Failed to re-assign stdout to logfile: %v", err)
+		log.Printf("Failed to re-assign stdout to logfile: %v", err)
 	}
 	if err := unix.Dup2(int(logf.Fd()), 2); err != nil {
 		// Not considered fatal
-		Errorf("Failed to re-assign stderr to logfile: %v", err)
+		log.Printf("Failed to re-assign stderr to logfile: %v", err)
 	}
 }

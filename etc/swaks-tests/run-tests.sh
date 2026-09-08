9663b0622521a2a6b5b31603f9a3c99039af7eae
James Hillyerd <james@hillyerd.com>
2026-09-08 12:03:14 -0700

    fix: outdated swaks syntax, missing DecodeHeader failure logging (#616)
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- etc/swaks-tests/run-tests.sh
+++ etc/swaks-tests/run-tests.sh
@@ -28,41 +28,41 @@ export SWAKS_OPT_server="${SWAKS_OPT_server:-127.0.0.1:2500}"
 export SWAKS_OPT_to="$to@inbucket.local"
 
 # Basic test
-swaks $* --h-Subject: "Swaks Plain Text" --body text.txt
+swaks $* --h-Subject: "Swaks Plain Text" --body @text.txt
 
 # Multi-recipient test
 swaks $* --to="$to@inbucket.local,alternate@inbucket.local" --h-Subject: "Swaks Multi-Recipient" \
-  --body text.txt
+  --body @text.txt
 
 # HTML test
-swaks $* --h-Subject: "Swaks HTML" --data mime-html.raw
+swaks $* --h-Subject: "Swaks HTML" --data @mime-html.raw
 
 # Top level HTML test
-swaks $* --h-Subject: "Swaks Top Level HTML" --data nonmime-html.raw
+swaks $* --h-Subject: "Swaks Top Level HTML" --data @nonmime-html.raw
 
 # Attachment test
 swaks $* --h-Subject: "Swaks Attachment" --attach-type image/png --attach favicon.png \
-  --body text.txt
+  --body @text.txt
 
 # Encoded subject line test
-swaks $* --data utf8-subject.raw
+swaks $* --data @utf8-subject.raw
 
 # Gmail test
-swaks $* --data gmail.raw
+swaks $* --data @gmail.raw
 
 # Outlook test
-swaks $* --data outlook.raw
+swaks $* --data @outlook.raw
 
 # Non-mime responsive HTML test
-swaks $* --data nonmime-html-responsive.raw
-swaks $* --data nonmime-html-inlined.raw
+swaks $* --data @nonmime-html-responsive.raw
+swaks $* --data @nonmime-html-inlined.raw
 
 # Incorrect charset, malformed final boundary
-swaks $* --data mime-errors.raw
+swaks $* --data @mime-errors.raw
 
 # IP RCPT domain
-swaks $* --to="swaks@[127.0.0.1]" --h-Subject: "IPv4 RCPT Address" --body text.txt
-swaks $* --to="swaks@[IPv6:2001:db8:aaaa:1::100]" --h-Subject: "IPv6 RCPT Address" --body text.txt
+swaks $* --to="swaks@[127.0.0.1]" --h-Subject: "IPv4 RCPT Address" --body @text.txt
+swaks $* --to="swaks@[IPv6:2001:db8:aaaa:1::100]" --h-Subject: "IPv6 RCPT Address" --body @text.txt
 
 # Inline attachment test
-swaks $* --data mime-inline.raw
+swaks $* --data @mime-inline.raw
